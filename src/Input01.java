5caf1af2ea6e4983e2c4954511301ee84ed82244
Aluno <Aluno@10.85.198.41>
2024-06-04 16:38:16 -0300

    Commit project Input01

---
+++ src/Input01.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JOptionPane;
+
+
+public class Input01 {
+    public static void main(String[] args) {
+        //Create a JOptionPane.
+        JOptionPane.showInputDialog("Test, escreva ai");
+        
+        
+        //Store the input as a String and print it.
+        //Parse the input as an int
+        
+        
+        
+        
+        
+    }
+}
